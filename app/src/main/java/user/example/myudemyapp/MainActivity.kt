d394bb3449521e7ad32a4fb8cf5b18f9cc7ff1e7
JafaMuhammad <jmohsa@gmail.com>
2020-05-09 03:36:02 +0300

    volley string update

--- app/src/main/java/user/example/myudemyapp/MainActivity.kt
+++ app/src/main/java/user/example/myudemyapp/MainActivity.kt
@@ -1,10 +1,6 @@
 package user.example.myudemyapp
-import android.content.Context
-import android.content.Intent
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
@@ -12,20 +8,21 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
 
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var url:String="https://10.0.2.2:80/UdemyWeb/greeting.php"
-        var rq:RequestQueue= Volley.newRequestQueue(this)
-        var sr=StringRequest(Request.Method.GET, url, Response.Listener { response ->
-            textView4.text=response
-        }, Response.ErrorListener { error ->
-            textView4.text=error.message
-        })
-        rq.add(sr)
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        var url: String = "http://10.0.2.2:8888/UdemyWeb/greeting.php/"
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                textView4.text = response
+            },
+            Response.ErrorListener { textView4.text = "That didn't work!" })
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest)
     }
     }
